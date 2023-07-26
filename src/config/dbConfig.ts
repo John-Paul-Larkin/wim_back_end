b6ba7743699ec14bfe23e847eaa8aa11dcd858bb
john Paul Larkin <johnplarkin@gmail.com>
2023-07-26 20:34:18 +0100

    mysql password

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -3,10 +3,12 @@ import mysql from "mysql2";
 
 dotenv.config();
 
+console.info('process.env.DB_PASSWORD was ' + (!process.env.DB_PASSWORD ? 'NOT ' : '') + 'set')
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || '123456',
   // port: parseInt(process.env.PORT || "", 10),
   port: 3306,
   database: process.env.DB_NAME,
