c0eee95097d60aa2cc69ca00b28faa85aa98dc65
john Paul Larkin <johnplarkin@gmail.com>
2023-07-26 21:12:24 +0100

    pass

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -5,6 +5,8 @@ dotenv.config();
 
 // console.info('process.env.DB_PASSWORD was ' + (!process.env.DB_PASSWORD ? 'NOT ' : '') + 'set')
 
+console.log(process.env.DB_PASSWORD);
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
